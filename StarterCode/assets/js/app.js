667c88091eed30ce927e85d837b01c24a2a85809
matkuhlm <68480067+matkuhlm@users.noreply.github.com>
2021-01-19 21:39:33 -0500

    js update

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,68 @@
-// @TODO: YOUR CODE HERE!
+//set parameters
+var width = parseInt(d3.select("scatter").style("width"));
+var height = width - width / 4;
+var margin = 25;
+var labelArea = 120;
+var padBot = 50
+var padLeft = 50
+
+
+//make layout
+var svg = d3
+    .select("scatter")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("class", "chart")
+
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
